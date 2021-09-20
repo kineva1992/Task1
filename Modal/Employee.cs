d517dba7b7d57f1f8c4ed31de6342a898a4c989c
alexandex <kienva1992@gmail.com>
2021-09-20 21:25:49 +0300

    123

---
+++ Modal/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1.Modal
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public int DepartamentId { get; set; }
+        public int Chief_id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+    }
+}
